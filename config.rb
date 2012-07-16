23826d81638b5e1b14977c409f1fd2eb20686fa3
Eric Meyer <eric@oddbird.net>
2012-07-16 15:58:09 -0600

    Add fonts, sass organization

--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
 # Require any additional compass plugins here.
 require "susy"
+require "modular-scale"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
+fonts_dir = "fonts"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
